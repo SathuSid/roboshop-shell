ff57e213876634119253551971670827741184e0
SathuSid <sisindhriy123@gmail.com>
2024-12-27 09:49:59 +0530

    All changes in the project.

--- shipping.sh
+++ shipping.sh
@@ -9,9 +9,9 @@ cd /app
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 dnf install mysql -y
-mysql -h mysql.rdevopsb81.online -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql.rdevopsb81.online -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h mysql.rdevopsb81.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql.siddevsecops.icu -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql.siddevsecops.icu -uroot -pRoboShop@1 < /app/db/app-user.sql
+mysql -h mysql.siddevsecops.icu -uroot -pRoboShop@1 < /app/db/master-data.sql
 systemctl daemon-reload
 systemctl enable shipping
 systemctl restart shipping
