8d0bbb1e05ae04401865e443057449e30dd23608
SathuSid <sisindhriy123@gmail.com>
2024-12-27 22:46:37 +0530

    adding print heading function

--- common.sh
+++ common.sh
@@ -5,14 +5,20 @@ rm -f $log_file
 
 
 app_prerequisites(){
-  echo -e "$color Creating the roboshop user $no_color"
+  print_heading "Creating the roboshop user"
   useradd roboshop &>>$log_file
   echo
 
-  echo -e "$color Extracting the input for dispatch api $no_color"
+  print_heading "Extracting the input for dispatch api"
   rm -rf /app &>>$log_file
   mkdir /app &>>$log_file
   curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$log_file
   cd /app
   unzip /tmp/$app_name.zip &>>$log_file
 }
+
+print_heading(){
+  echo -e "$color $1 $no_color" &>>$log_file
+  echo -e "$color $1 $no_color"
+}
+
--- dispatch.sh
+++ dispatch.sh
@@ -1,23 +1,24 @@
 source common.sh
 app_name="dispatch"
 
-echo -e "$color Copying the dispatch service file $no_color"
+
+print_heading "Copying the dispatch service file"
 cp dispatch.service /etc/systemd/system/dispatch.service &>>$log_file
 echo $?
 
-echo -e "$color Installing the golang $no_color"
+print_heading "Installing the golang"
 dnf install golang -y &>>$log_file
 echo $?
 app_prerequisites
 
 cd /app
-echo -e "$color Download application dependencies $no_color"
+print_heading "Download application dependencies"
 go mod init dispatch &>>$log_file
 go get &>>$log_file
 go build &>>$log_file
 echo $?
 
-echo -e "$color restarting the dispatch service $no_color"
+print_heading "restarting the dispatch service"
 systemctl daemon-reload &>>$log_file
 systemctl enable dispatch &>>$log_file
 systemctl restart dispatch &>>$log_file
