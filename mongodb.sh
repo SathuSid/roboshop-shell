001f7e55777b32b20b252ed2db5d6809700034f1
SathuSid <sisindhriy123@gmail.com>
2024-12-28 14:47:07 +0530

    adding roles to services

--- mongodb.sh
+++ mongodb.sh
@@ -5,12 +5,13 @@ print_heading "copy mongodb repo file"
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
 status_check $?
 
-print_heading "update the listener address"
-sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$log_file
-status_check $?
 
 print_heading "install mongodb"
 dnf install mongodb-org -y &>>$log_file
+status_check $?
+
+print_heading "update the listener address"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$log_file
 status_check $?
 
 print_heading "enable and restart mongodb service"
